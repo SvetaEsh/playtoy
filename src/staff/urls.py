3cbc1ac365c5bcd36e36e43cbbf56946b6b6bd5c
Sviatlana E <Svetic010289@gmail.com>
2023-06-28 10:42:53 +0300

    add login on base.html + view signup

--- src/staff/urls.py
+++ src/staff/urls.py
@@ -5,4 +5,5 @@ app_name='staff'
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name="login"),
     path('logout/', views.LogoutView.as_view(), name="logout"),
+    path('signup/', views.SignUp.as_view(), name="signup"),
     ]
--- src/staff/views.py
+++ src/staff/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 # Create your views here.
 class LoginView(auth_views.LoginView):
     template_name = 'staff/login.html'
 
 class LogoutView(auth_views.LogoutView):
     template_name='staff/logout.html'
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("staff:login")
+    template_name = "staff/signup.html"
