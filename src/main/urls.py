851cc797cf3634fc02a12d7523228309a4856efb
Sviatlana E <Svetic010289@gmail.com>
2023-07-02 19:47:03 +0300

    remake links and info

--- src/main/urls.py
+++ src/main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='main')
-    
+    path('', views.HomePage.as_view(), name='main'),
+    path('delivery', views.Delivery.as_view(), name='delivery'),
+    path('contact', views.Contact.as_view(), name='contact')
     ]
--- src/main/views.py
+++ src/main/views.py
@@ -6,3 +6,10 @@ from . import models
 # Create your views here.
 class HomePage(generic.TemplateView):
     template_name="main/home-page.html"
+
+
+class Delivery(generic.TemplateView):
+    template_name="main/shipping-and-payment.html"
+
+class Contact(generic.TemplateView):
+    template_name="main/contact.html"    
