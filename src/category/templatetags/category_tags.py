fa3e354cf7cbfce60a779353e9c4cdedb57806d7
Sviatlana E <Svetic010289@gmail.com>
2023-07-04 17:09:20 +0300

    fix base teplate for goods

--- src/category/templatetags/category_tags.py
+++ src/category/templatetags/category_tags.py
@@ -2,7 +2,6 @@ from django import template
 from category.models import Category
 register = template.Library()
 
-
 @register.inclusion_tag('category/menu-category.html', takes_context=True)
 def show_top_menu(context):
     menu_items = Category.objects.all()
