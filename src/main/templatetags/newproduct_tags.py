6cdd8f8a9e300478ad7414c789ee8bb3ee7cdca5
Sviatlana E <Svetic010289@gmail.com>
2023-07-04 21:14:46 +0300

    add newproduct

---
+++ src/main/templatetags/newproduct_tags.py
@@ -0,0 +1,10 @@
+from django import template
+from product.models import Product
+register = template.Library()
+
+@register.inclusion_tag('main/newproduct-list.html', takes_context=True)
+def show_top_newproduct(context, count=12):
+    newproduct_items = Product.objects.order_by('-created')[:count]
+    return {
+        "newproduct_items": newproduct_items,
+    }
