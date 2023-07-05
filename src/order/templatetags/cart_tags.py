d7a036ea5d106ef1a8c690eb85bed89fc3ae57db
Sviatlana E <Svetic010289@gmail.com>
2023-07-06 00:32:47 +0300

    fix

--- src/order/templatetags/cart_tags.py
+++ src/order/templatetags/cart_tags.py
@@ -6,8 +6,16 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def total_quantity(context):
     request = context['request']
+    print('request ',request)
     cart_pk = request.session.get('cart_pkid')
-    cart = Cart.objects.get(pk=cart_pk)
-    return cart.total_quantity
+    print('cart_pk ',cart_pk)
+    if cart_pk:
+        cart = Cart.objects.get(pk=cart_pk)
+        print('cart ', cart)
+        print(cart.total_quantity)
+        return cart.total_quantity
+    else:
+        print("cart is none!!!")
+        return ""
 
 
